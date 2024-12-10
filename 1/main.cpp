2406a8b794a82f38542267444e5c4454140b5ad6
Liza <maria.enytina129@gmail.com>
2024-12-10 22:39:23 +0500

    the framework of the program

--- 1/main.cpp
+++ 1/main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+vector<string> read_lines_from_file(const string& file) {
+  vector<string> lines;
+  return lines;
+}
+
+void print_lines(const vector<string>& lines) {
+}
+
+void write_lines_to_file(const vector<string>& lines, const string& file) {
+}
+
+int main() {
+  string input_file = "input.txt";
+  string output_file = "output.txt";
+
+  vector<string> lines = read_lines_from_file(input_file);
+  print_lines(lines);
+  write_lines_to_file(lines, output_file);
+
+  return 0;
 }
