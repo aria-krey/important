fe5cf756c72bfca6b78beb6f8f2b3b891b173241
Liza <maria.enytina129@gmail.com>
2024-12-11 00:20:27 +0500

    Ready

--- 1/main.cpp
+++ 1/main.cpp
@@ -4,20 +4,15 @@
 #include <string>
 
 #include "read_file.h"
+#include "write_and_print_file.h"
 
 using namespace std;
-
-void print_lines(const vector<string>& lines) {
-}
-
-void write_lines_to_file(const vector<string>& lines, const string& file) {
-}
 
 int main() {
   string input_file = "input.txt";
   string output_file = "output.txt";
 
-  vector<string> lines = read_lines_from_file(input_file);
+  //vector<string> lines = {"Line 1", "Line 2", "Line 3"};
   print_lines(lines);
   write_lines_to_file(lines, output_file);
 
